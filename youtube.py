21c109f0d4e765399858b2d1cee16e3e9f4434df
mak1143 <206786647+mak1143@users.noreply.github.com>
2026-02-19 02:42:47 +0000

    finished downloader

--- youtube.py
+++ youtube.py
@@ -1,29 +1,17 @@
+import os
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
 
-# def download_video(inputUrl, yt , ys ,dst):
-#     inputUrl = input("please enter youtube url>: ")
-#     yt = YouTube(url, on_progress_callback=on_progress)
-#
-#     ys = yt.streams.get_highest_resolution()
-#
-#     dst = output("home/shoyo/Downloads")
-#     ys.download(dst)
-#     print(f"Completed! file saved to: {dst}")
-#
-#
-# download_video(inputUrl,yt,ys,dst)
-#
 def download_video():
     inputUrl = input("please enter youtube url>: ")
-    yt = YouTube(url, on_progress_callback=on_progress)
+    yt = YouTube(inputUrl, on_progress_callback=on_progress)
 
     ys = yt.streams.get_highest_resolution()
 
-    dst = output("home/shoyo/Downloads")
-    ys.download(dst)
-    print(f"Completed! file saved to: {dst}")
+    dst = os.path.expanduser("~/Downloads/")
+    ys.download(output_path=dst)
+    print(f"Completed! {yt.title} >: file saved to: {dst}")
 
 
 download_video()
