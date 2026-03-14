f9ddcf15a405789eebf617d2d9bb2375d52387f7
mak1143 <206786647+mak1143@users.noreply.github.com>
2026-03-14 11:08:12 +0000

    New feature exception key method

--- youtube.py
+++ youtube.py
@@ -1,18 +1,22 @@
-import os
+import os, sys
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
 
 def download_video():
-    print("Please use youtube music for audio")
-    inputUrl = input("please enter youtube url>: ")
-    yt = YouTube(inputUrl, on_progress_callback=on_progress)
+    try:
+        print("Please use youtube music for audio")
+        inputUrl = input("please enter youtube url>: ")
+        yt = YouTube(inputUrl, on_progress_callback=on_progress)
 
-    ys = yt.streams.get_highest_resolution()
+        ys = yt.streams.get_highest_resolution()
 
-    dst = os.path.expanduser("~/Downloads/")
-    ys.download(output_path=dst)
-    print(f"Completed! {yt.title} >: file saved to: {dst}")
+        dst = os.path.expanduser("~/Downloads/")
+        ys.download(output_path=dst)
+        print(f"Completed! {yt.title} >: file saved to: {dst}")
+
+    except KeyboardInterrupt:
+        sys.exit()
 
 
 download_video()
